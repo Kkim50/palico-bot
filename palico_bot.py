976c2941033521c457258d33a229733811103720
Kathy <kim.kathleen97@gmail.com>
2020-10-18 17:00:19 -0400

    Now work with command key village number

--- palico_bot.py
+++ palico_bot.py
@@ -1,34 +1,41 @@
 import discord
 import requests
+from discord.ext import commands
 from bs4 import BeautifulSoup
+from bs import try_load_html_as_soup
 
+filename = 'data/quests.html'
 URL = 'https://mhgu.kiranico.com/quest'
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find(id='s0-1')
-tr_list = results.find_all('tr')
-tds = []
-quest_infos = []
-key_quests = []
-i = 0
-for i in range(0, len(tr_list), 2):
-    quest_infos.append(tr_list[i].find_all('td') + tr_list[i+1].find_all('td'))
-client = discord.Client()
+soup = try_load_html_as_soup(filename, URL)
 
-key_quests = []
-for quest in quest_infos:
-    if quest[0].find(text='Key'):
-        quest_name = quest[1].find('a').contents[0].strip()
-        key_quests.append(quest_name)
-print(key_quests)
+bot = commands.Bot(command_prefix='!')
 
-@client.event
+def findVillageQuests(id):
+    results = soup.find(id='s0-'+id)
+    tr_list = results.find_all('tr')
+    quest_infos = []
+    key_quests = []
+    i = 0
+    for i in range(0, len(tr_list), 2):
+        quest_infos.append(tr_list[i].find_all(
+            'td') + tr_list[i+1].find_all('td'))
+    for quest in quest_infos:
+        if quest[0].find(text='Key'):
+            quest_name = quest[1].find('a').contents[0].strip()
+            key_quests.append(quest_name)
+    return key_quests
+
+def findHubQuests(id):
+    results = soup.find(id="")
+
+@bot.event
 async def on_ready():
-    print('{} has connected to Discord!'.format(client.user))
+    print('{} has connected to Discord!'.format(bot.user))
 
-@client.event
-async def on_message(message):
-    if message.content.startswith('!Keyquest'):
-        await message.channel.send(key_quests)
+@bot.command("key")
+async def keyquest(ctx, type, id):
+    if type == "village" or "": 
+        keyquests = findVillageQuests(id)
+        await ctx.send(keyquests)
 
-client.run(token)
+bot.run(token)
