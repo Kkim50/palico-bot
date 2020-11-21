d61d1f8da37a3acee0cc5d6f26b3200281630752
Kathy <kim.kathleen97@gmail.com>
2020-11-21 18:19:14 -0500

    Updates

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
-import palico_bot
+import plain_palico_bot
 
 app = Flask(__name__)
 
@@ -8,6 +8,6 @@ app = Flask(__name__)
 @cross_origin()
 def api_post():
     if (request.method == 'POST'):
-        return jsonify(palico_bot.response)
+        return jsonify(plain_palico_bot.response)
     else:
         return "Error!"
